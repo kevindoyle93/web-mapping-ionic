a129f1bdf32b22a15c5d6e991911506fe188291f
Kevin Doyle <C13547287@mydit.ie>
2017-04-28 11:46:38 +0100

    Use map service to display leaflet map

--- src/pages/club-detail/club-detail.ts
+++ src/pages/club-detail/club-detail.ts
@@ -1,52 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Club } from '../../api/models/club';
+import { MapService } from '../../providers/map-service';
 import * as Leaflet from 'leaflet';
 
 @Component({
   selector: 'page-club-detail',
   templateUrl: 'club-detail.html',
+  providers: [MapService]
 })
 export class ClubDetail implements OnInit {
 
   club: Club;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public mapService: MapService) {
     this.club = navParams.get('club');
   }
 
   ngOnInit(): void {
-    this.drawMap();
-  }
-
-  drawMap(): void {
-    let map = Leaflet.map('map');
-
-    Leaflet.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicGF0cmlja3IiLCJhIjoiY2l2aW9lcXlvMDFqdTJvbGI2eXUwc2VjYSJ9.trTzsdDXD2lMJpTfCVsVuA', {
-      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-      maxZoom: 18
-    }).addTo(map);
-
-    //web location
-    map.locate({ setView: true});
-
-    //when we have a location draw a marker and accuracy circle
-    function onLocationFound(e) {
-      let radius = e.accuracy / 2;
-
-      Leaflet.marker(e.latlng).addTo(map)
-        .bindPopup("You are within " + radius + " meters from this point").openPopup();
-
-      Leaflet.circle(e.latlng, radius).addTo(map);
-    }
-    map.on('locationfound', onLocationFound);
-    //alert on location error
-    function onLocationError(e) {
-      alert(e.message);
-    }
-
-    map.on('locationerror', onLocationError);
-
+    this.mapService.createMap('map', this.club.clubhouseLocation[1], this.club.clubhouseLocation[0]);
+    this.mapService.addPoint(this.club.clubhouseLocation[1], this.club.clubhouseLocation[0], 15);
   }
 
 }
--- src/providers/map-service.ts
+++ src/providers/map-service.ts
@@ -1,18 +1,28 @@
 import {Injectable} from "@angular/core";
-import {Http} from "@angular/http";
-import {Map} from "leaflet";
-import 'leaflet';
+import * as Leaflet from 'leaflet';
+import LatLng = L.LatLng;
 
 @Injectable()
 export class MapService {
-  public map: Map;
+  public map: any;
   public baseMaps: any;
 
-  constructor(private http: Http) {
-    this.baseMaps = {
-      OpenStreetMap: L.tileLayer("http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>'
-      })
-    };
+  constructor() {
+
   }
+
+  public createMap = (elementId: string, lat: number, lng: number) => {
+    this.map = Leaflet.map(elementId);
+
+    Leaflet.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicGF0cmlja3IiLCJhIjoiY2l2aW9lcXlvMDFqdTJvbGI2eXUwc2VjYSJ9.trTzsdDXD2lMJpTfCVsVuA', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18
+    }).addTo(this.map);
+
+    this.map.setView(new LatLng(lat, lng), 14);
+  };
+
+  public addPoint = (lat: number, lng: number, radius: number) => {
+    Leaflet.circle(new LatLng(lat, lng), radius).addTo(this.map);
+  };
 }
