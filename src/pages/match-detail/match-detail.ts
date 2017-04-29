1d0bec499ade89cad6dd0b6c6f5594b23a4afa67
Kevin Doyle <C13547287@mydit.ie>
2017-04-29 12:21:14 +0100

    Get user location in match detail

--- src/pages/match-detail/match-detail.ts
+++ src/pages/match-detail/match-detail.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 import { Match } from '../../api/models/match';
+import { MapService } from '../../providers/map-service';
 
 @Component({
   selector: 'page-match-detail',
   templateUrl: 'match-detail.html',
+  providers: [Geolocation, MapService]
 })
 export class MatchDetail {
 
   match: Match;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public geolocation: Geolocation, public  mapService: MapService) {
     this.match = navParams.get('match');
+
+    geolocation.getCurrentPosition().then((data) => {
+      console.log('My latitude : ', data.coords.latitude);
+      console.log('My longitude: ', data.coords.longitude);
+    });
   }
 
 }
